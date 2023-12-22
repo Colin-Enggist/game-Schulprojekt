848fdcfa41ed82712cfc96fa57eb31839115f2de
Colin <colin.enggist@gmail.com>
2023-12-22 11:38:10 +0100

    Screen Setup finished

--- src/screen.js
+++ src/screen.js
@@ -1,5 +1,24 @@
 export class Screen{
+    constructor(){
+        this.setup();
+    }
+
+    setup= async ()=>{
+        this.canvas =  document.getElementById("canvas");
+
+
+        this.setScreenSize();
+    }
     run(){
         console.log("running");
+    }
+
+    setScreenSize(){
+        let w = window.innerWidth;
+        let h = window.innerHeight;
+        this.canvas.width = w; 
+        this.canvas.height= h;
+         
+
     }
 }
